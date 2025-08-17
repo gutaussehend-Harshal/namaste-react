ab3d4d7380b0ef18f6cef27c760fd60144e9386c
Harshal Amrut Patil <harshal.patil@gmail.com>
2025-08-17 19:34:28 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = <h1 className="head">Namaste React Using JSX 🚀</h1>;
+const Title = () => <h1 className="head">Namaste React Using JSX 🚀</h1>;
 
-// Class Based Components - OLD
-// Functional Components - NEW
-
-// React Functional Component
-const HeadingComponent1 = () => {
-  return <h1 className="heading">Namaste React Functional Component 🚀</h1>;
-};
-
-const HeadingComponent2 = () => (
+// Component Composition
+const HeadingComponent = () => (
   <div id="container">
-    <h1 className="heading">Namaste React Functional Component 🚀</h1>
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Namaste React Functional Component</h1>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(<HeadingComponent1 />);
+root.render(<HeadingComponent />);
